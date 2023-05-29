036793ca268055726aab8b17d1aa1b05b94db2ca
marmelo <marmeloF@noreply>
2023-05-29 12:05:15 +0100

    function to add games

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sqlite3
 
 database_path = "./online_game_shop.db"
 con = sqlite3.connect(database_path)
-cur = con.Cursor()
+cur = con.cursor()
 
 app = FastAPI()
 
@@ -11,6 +11,18 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/insert_games")
+async def insert_games(title: str, genre: str, price: int, age_range: int):
+    query = f"""
+        INSERT INTO Game(title, genre, price, age_range)
+        VALUES({title}, {genre}, {price}, {age_range})
+    """
+    cur.execute(query)
+    con.commit()
+
+    return {"Query completed"}
 
 
 # this closes the connection to the database made with sqlite3
