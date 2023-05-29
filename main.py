55e8f0df028511066b95b08191eca4ccb6fbc21a
marmelo <marmeloF@noreply>
2023-05-29 21:04:08 +0100

    inserts para as tabelas feitas

--- main.py
+++ main.py
@@ -13,15 +13,67 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.post("/insert_games")
-async def insert_games(title: str, genre: str, price: int, age_range: int):
+@app.post("/insert_game")
+async def insert_game(
+        title: str,
+        genre: str,
+        price: int = 60,
+        age_range: int = 18
+        ):
+
     query = f"""
         INSERT INTO Game(title, genre, price, age_range)
         VALUES({title}, {genre}, {price}, {age_range})
     """
     cur.execute(query)
     con.commit()
+    return {"Query completed"}
 
+
+@app.post("/insert_gamer")
+async def insert_gamer(
+        name: str,
+        nationality: str,
+        age: int = 0
+        ):
+    query = f"""
+        INSERT INTO Gamer (name, age, nationality)
+        VALUES({name}, {age}, {nationality});
+    """
+    cur.execute(query)
+    con.commit()
+    return {"Query completed"}
+
+
+@app.post("/insert_purchase")
+async def insert_purchase(
+        gamer_id: int,
+        game_id: int,
+        price: int,
+        purchase_date: str
+        ):
+    query = f"""
+        INSERT INTO Purchase (gamer_id, game_id, price, purchase_date)
+        VALUES({gamer_id}, {game_id}, {price}, {purchase_date})
+    """
+    cur.execute(query)
+    con.commit()
+    return {"Query completed"}
+
+
+@app.post("/insert_review")
+async def insert_review(
+        gamer_id: int,
+        game_id: int,
+        message: str,
+        rating: int
+        ):
+    query = f"""
+        INSERT INTO Review (gamer_id, game_ind, message, rating)
+        VALUES({gamer_id}, {game_id}, {message}, {rating})
+    """
+    cur.execute(query)
+    con.commit()
     return {"Query completed"}
 
 
