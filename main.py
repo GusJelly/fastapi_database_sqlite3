5cdd832dfd73f6e096af0f72dcafdb4764c4fe4b
marmelo <marmeloF@noreply>
2023-05-30 19:27:21 +0100

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import sqlite3
+import logging
 
 database_path = "./online_game_shop.db"
 con = sqlite3.connect(database_path)
@@ -7,9 +8,10 @@ cur = con.cursor()
 
 app = FastAPI()
 
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
 
-# Get endpoints for retrieving info from the database and transmiting it
-# through the api
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -20,81 +22,106 @@ async def gamer(name: str, age: int = 0, nationality: str = "portugal"):
     query = f"""
         SELECT * FROM Gamer WHERE name = {name};
     """
-    query_data = cur.execute(query)
+    try:
+        cur.execute(query, (name))
+        query_data = cur.fetchone()
 
-    return {
-            "name": query_data[1],
-            "age": query_data[2],
-            "nationality": query_data[3]
-    }
+        if query_data:
+            return {
+                "name": query_data[1],
+                "age": query_data[2],
+                "nationality": query_data[3]
+            }
+        else:
+            return {"message": "Gamer not found"}
+    except Exception as e:
+        logging.error(f"Error retrieving gamer: {e}")
+        return {"message": "Internal server error"}
 
 
-# Post endpoints for inserting information through the api
-@app.post("/insert_game")
+@app.post("/insert_game/")
 async def insert_game(
-        title: str,
-        genre: str,
-        price: int = 60,
-        age_range: int = 18
-        ):
-
+    id: int,
+    title: str,
+    genre: str,
+    price: int = 60,
+    age_range: int = 18
+):
     query = f"""
-        INSERT INTO Game(title, genre, price, age_range)
-        VALUES({title}, {genre}, {price}, {age_range})
+        INSERT INTO Game(id, title, genre, price, age_range)
+        VALUES({id}, {title}, {genre}, {price}, {age_range})
     """
-    cur.execute(query)
-    con.commit()
-    return {"Query completed"}
+    try:
+        cur.execute(query, (title, genre, price, age_range))
+        con.commit()
+        return {"message": "Game inserted successfully"}
+    except Exception as e:
+        logging.error(f"Error inserting game: {e}")
+        return {"message": "Internal server error"}
 
 
 @app.post("/insert_gamer/")
 async def insert_gamer(
-        name: str,
-        nationality: str,
-        age: int = 0
-        ):
+    id: int,
+    name: str,
+    nationality: str,
+    age: int = 0
+):
     query = f"""
-        INSERT INTO Gamer (name, age, nationality)
-        VALUES({name}, {age}, {nationality});
+        INSERT INTO Gamer (id, name, age, nationality)
+        VALUES({id}, {name}, {nationality}, {age});
     """
-    cur.execute(query)
-    con.commit()
-    return {"Query completed"}
+    try:
+        cur.execute(query, (name, age, nationality))
+        con.commit()
+        return {"message": "Gamer inserted successfully"}
+    except Exception as e:
+        logging.error(f"Error inserting gamer: {e}")
+        return {"message": "Internal server error"}
 
 
 @app.post("/insert_purchase/")
 async def insert_purchase(
-        gamer_id: int,
-        game_id: int,
-        price: int,
-        purchase_date: str
-        ):
+    id: int,
+    gamer_id: int,
+    game_id: int,
+    price: int,
+    purchase_date: str
+):
     query = f"""
-        INSERT INTO Purchase (gamer_id, game_id, price, purchase_date)
-        VALUES({gamer_id}, {game_id}, {price}, {purchase_date})
+        INSERT INTO Purchase (id, gamer_id, game_id, price, purchase_date)
+        VALUES({id}, {gamer_id}, {game_id}, {price}, {purchase_date})
     """
-    cur.execute(query)
-    con.commit()
-    return {"Query completed"}
+    try:
+        cur.execute(query, (gamer_id, game_id, price, purchase_date))
+        con.commit()
+        return {"message": "Purchase inserted successfully"}
+    except Exception as e:
+        logging.error(f"Error inserting purchase: {e}")
+        return {"message": "Internal server error"}
 
 
 @app.post("/insert_review/")
 async def insert_review(
-        gamer_id: int,
-        game_id: int,
-        message: str,
-        rating: int
-        ):
+    id: int,
+    gamer_id: int,
+    game_id: int,
+    message: str,
+    rating: int
+):
     query = f"""
-        INSERT INTO Review (gamer_id, game_ind, message, rating)
-        VALUES({gamer_id}, {game_id}, {message}, {rating})
+        INSERT INTO Review (id, gamer_id, game_id, message, rating)
+        VALUES({id}, {gamer_id}, {game_id}, {message}, {rating})
     """
-    cur.execute(query)
-    con.commit()
-    return {"Query completed"}
+    try:
+        cur.execute(query, (gamer_id, game_id, message, rating))
+        con.commit()
+        return {"message": "Review inserted successfully"}
+    except Exception as e:
+        logging.error(f"Error inserting review: {e}")
+        return {"message": "Internal server error"}
 
 
-# this closes the connection to the database made with sqlite3
 @app.on_event("shutdown")
 async def shutdown():
     con.close()
